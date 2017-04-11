3681f4e633540318ced37f534a343e24d075b33e
Georg Moiseenko <Georg_Moiseenko@epam.com>
2017-04-11 17:50:58 +0300

    Web infrastructure added

---
+++ MusicNet.Web/README.md
@@ -0,0 +1,3 @@
+﻿# MusicNet.Web
+
+
---
+++ MusicNet.Web/webpack.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+    entry: "./app/app.jsx",
+    output: {
+        path: "public",
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    }
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/
+                loader: ["babel-loader"]
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+}
